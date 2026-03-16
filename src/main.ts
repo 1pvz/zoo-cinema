18105daa4bed3026efbf85c78690e27d4fa9b6ba
1pvz <lintian255@outlook.com>
2026-03-16 18:35:57 +0800

    添加小动物模型

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import ZooEngine from "./ZooEngine"
 import CinemaScene from "./scene/CinemaScene"
 import Environment from "./scene/Environment"
+import ModelLoader from "./scene/ModelLoader"
 
-const __main = () => {
+const __main = async () => {
     const canvas = document.getElementById('id-render-canvas') as HTMLCanvasElement
     const engine = ZooEngine.init(canvas)
     const cinema = new CinemaScene(engine)
     new Environment(cinema.scene)
+
+    const modelLoader = new ModelLoader(cinema.scene)
+    await modelLoader.loadAnimals()
+    console.log('load animals', modelLoader.names)
+
     engine.startRenderLoop(cinema.scene)
 }
 
