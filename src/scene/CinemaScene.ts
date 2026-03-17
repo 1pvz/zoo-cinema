c3e14d18f6f66864640b694e40e398f0a3c8e47b
1pvz <lintian255@outlook.com>
2026-03-17 13:53:56 +0800

    播放视频，加上暂停功能

--- src/scene/CinemaScene.ts
+++ src/scene/CinemaScene.ts
@@ -4,12 +4,21 @@ import {
     Vector3,
     ArcRotateCamera,
     HemisphericLight,
+    DirectionalLight,
+    CascadedShadowGenerator,
+    ShadowGenerator,
+    GrainPostProcess,
+    TonemapPostProcess,
+    TonemappingOperator,
+    DefaultRenderingPipeline,
+    SSAORenderingPipeline,
 } from "@babylonjs/core"
 import type ZooEngine from "../ZooEngine"
 
 export default class CinemaScene {
     readonly scene: Scene
     readonly camera: ArcRotateCamera
+    readonly cameraVideo: ArcRotateCamera
 
     constructor(zooEngine: ZooEngine) {
         this.scene = new Scene(zooEngine.engine)
@@ -18,9 +27,15 @@ export default class CinemaScene {
         this.camera = this._createCamera('camera', zooEngine.canvas)
         this.camera.layerMask = 0b01
 
+        this.cameraVideo = this._createCamera('cameraVideo', zooEngine.canvas)
+        this.cameraVideo.layerMask = 0b10
+
+
         this.scene.activeCameras?.push(this.camera)
+        this.scene.activeCameras?.push(this.cameraVideo)
 
         this._setupLights()
+        this._setupPostProcess()
     }
 
     private _createCamera(name: string, canvas: HTMLCanvasElement): ArcRotateCamera {
@@ -37,5 +52,42 @@ export default class CinemaScene {
     private _setupLights() {
         const ambient = new HemisphericLight('ambient', new Vector3(0, 1, 0), this.scene)
         ambient.intensity = 0.5
+
+        const shadowLight = new DirectionalLight('shadowLight', new Vector3(-1, -2, -1), this.scene)
+        shadowLight.position = new Vector3(320, 570, 370)
+        shadowLight.intensity = 0.8
+
+        const sg = new CascadedShadowGenerator(2048, shadowLight)
+        sg.filter = ShadowGenerator.FILTER_PCF
+        sg.bias = 0.0002
+
+        const animalLight = new DirectionalLight('animalLight', new Vector3(-1, -2, -1), this.scene)
+        animalLight.position = new Vector3(320, 570, 370)
+        animalLight.intensity = 0.4
+    }
+
+    private _setupPostProcess() {
+        const camera = this.camera
+
+        const grain = new GrainPostProcess('grain', 1, camera)
+        grain.samples = 4
+        grain.intensity = 5
+
+        new TonemapPostProcess('tonemap', TonemappingOperator.Photographic, 2.5, camera)
+
+        const pipeline = new DefaultRenderingPipeline('default', true, this.scene, [camera])
+        pipeline.bloomEnabled = true
+        pipeline.bloomThreshold = 0.05
+        pipeline.bloomWeight = 0.3
+        pipeline.bloomKernel = 64
+        pipeline.bloomScale = 0.5
+        pipeline.imageProcessingEnabled = false
+
+        const ssao = new SSAORenderingPipeline('ssao', this.scene, 0.75, [camera])
+        ssao.totalStrength = 1.35
+        ssao.base = 0.80
+        ssao.radius = 0.0006
+        ssao.area = 0.0015
+        ssao.fallOff = 0.0000001
     }
 }
---
+++ src/scene/VideoScreen.ts
@@ -0,0 +1,113 @@
+import {
+    Scene,
+    Color3,
+    Vector3,
+    MeshBuilder,
+    StandardMaterial,
+    VideoTexture,
+    Mesh,
+} from "@babylonjs/core"
+import { AdvancedDynamicTexture, Ellipse, TextBlock } from "@babylonjs/gui"
+
+export default class VideoScreen {
+    private readonly _plane: Mesh
+    private readonly _videoTexture: VideoTexture
+    private readonly _pauseIcon: Ellipse
+    private _focused = false
+
+    constructor(scene: Scene, videoUrl: string) {
+        this._plane = MeshBuilder.CreatePlane('videoScreen', {
+            width: 160,
+            height: 90,
+            sideOrientation: Mesh.DOUBLESIDE,
+        }, scene)
+        this._plane.position = new Vector3(0, 50, 0)
+        this._plane.layerMask = 0b01
+
+        const material = new StandardMaterial('videoScreenMat', scene)
+        this._videoTexture = new VideoTexture('videoTex', videoUrl, scene)
+        material.diffuseTexture = this._videoTexture
+        material.roughness = 1
+        material.emissiveColor = Color3.White()
+        this._plane.material = material
+
+        this._pauseIcon = this._createPauseIcon(scene)
+
+        this._setupClickToggle(scene)
+        this._setupKeyToggle(scene)
+    }
+
+    private _createPauseIcon(scene: Scene): Ellipse {
+        const overlay = MeshBuilder.CreatePlane('videoOverlay', {
+            width: 160,
+            height: 90,
+            sideOrientation: Mesh.DOUBLESIDE,
+        }, scene)
+        overlay.position = new Vector3(0, 0, -0.1)
+        overlay.layerMask = 0b01
+        overlay.isPickable = false
+        overlay.parent = this._plane
+
+        const guiTexture = AdvancedDynamicTexture.CreateForMesh(overlay, 1024, 576)
+        guiTexture.hasAlpha = true
+
+        const circle = new Ellipse('pauseCircle')
+        circle.width = '120px'
+        circle.height = '120px'
+        circle.color = 'white'
+        circle.thickness = 4
+        circle.background = 'rgba(0, 0, 0, 0.5)'
+        circle.isVisible = false
+        guiTexture.addControl(circle)
+
+        const icon = new TextBlock('pauseText', '▶')
+        icon.color = 'white'
+        icon.fontSize = 60
+        icon.paddingLeft = '10px'
+        circle.addControl(icon)
+
+        return circle
+    }
+
+    private _setupClickToggle(scene: Scene): void {
+        this._plane.isPickable = true
+
+        scene.onPointerDown = () => {
+            const pickResult = scene.pick(scene.pointerX, scene.pointerY, (mesh) => mesh === this._plane)
+            if (pickResult?.hit) {
+                this._focused = true
+                this._toggle()
+            } else {
+                this._focused = false
+            }
+        }
+    }
+
+    private _setupKeyToggle(scene: Scene): void {
+        scene.onKeyboardObservable.add((kbInfo) => {
+            if (this._focused && kbInfo.type === 2 && kbInfo.event.code === 'Space') {
+                kbInfo.event.preventDefault()
+                this._toggle()
+            }
+        })
+    }
+
+    private _toggle(): void {
+        const video = this._videoTexture.video
+        if (video.paused) {
+            video.play()
+            this._pauseIcon.isVisible = false
+        } else {
+            video.pause()
+            this._pauseIcon.isVisible = true
+        }
+    }
+
+    play(): void {
+        this._videoTexture.video.play()
+    }
+
+    pause(): void {
+        this._videoTexture.video.pause()
+    }
+}
